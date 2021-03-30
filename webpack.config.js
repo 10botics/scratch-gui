48a835c3f9fac651bc3d3e6d8bd8f7131c42ac5c
Jonathan Tse <jonathan@tsez.net>
2021-03-31 01:04:03 +0800

    1. Add better dev params in webpack 2. Fix iOS camera issue

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,9 @@ const base = {
     devServer: {
         contentBase: path.resolve(__dirname, 'build'),
         host: '0.0.0.0',
-        port: process.env.PORT || 8601
+        port: process.env.PORT || 8601,
+        disableHostCheck: true,
+        https: true
     },
     output: {
         library: 'GUI',
